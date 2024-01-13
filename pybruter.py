4b596d4511ab1fd04b53d792458ed7ccaa1f9557
simple-byte-coding <gagemorgan320@gmail.com>
2024-01-12 21:54:13 -0600

    Fixed syntax warnings and corrected except statements to look for the correct exceptions for better error output

--- pybruter.py
+++ pybruter.py
@@ -3,25 +3,25 @@ import threading
 
 
 def main():
-	url = input("| PYBRUTER | | Enter target url |~>: ")
-	wordlist = input("| PYBRUTER | | Wordlist |~>: ")
+    url = input("| PYBRUTER | | Enter target url |~>: ")
+    wordlist = input("| PYBRUTER | | Wordlist |~>: ")
 
-	try:
-		with open(wordlist, "r") as fp:
-			for word in fp:
-				url = (url + "/" + word)
-				try:
-					r = requests.get(url)
-					if r.status_code == 200:
-						print(f"{url} is real")
-					else:
-						continue
-				except:
-					continue
-	except:
-		print("No such wordlist!")
+    try:
+        with open(wordlist, "r") as fp:
+            for word in fp:
+                url = (url + "/" + word)
+                try:
+                    r = requests.get(url)
+                    if r.status_code == 200:
+                        print(f"{url} is real")
+                    else:
+                        continue
+                except ConnectionError:
+                    continue
+    except FileNotFoundError:
+        print("No such wordlist!")
 
 
 if __name__ == "__main__":
-	t1 = threading.Thread(target=main)
-	t1.start()
+    t1 = threading.Thread(target=main)
+    t1.start()
